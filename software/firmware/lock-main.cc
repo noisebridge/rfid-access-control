ecf8878c0dbfe98c203e4616cc89eb88aceeb036
Henner Zeller <h.zeller@acm.org>
2014-08-03 22:03:07 -0700

    o Implement simple serial protocol.

--- software/firmware/lock-main.cc
+++ software/firmware/lock-main.cc
@@ -1,118 +1,180 @@
+/* -*- mode: c++; c-basic-offset: 2; indent-tabs-mode: nil; -*- */
 
-#include <avr/eeprom.h>
 #include <avr/io.h>
 #include <string.h>
 #include <util/delay.h>
+#include <stdio.h>   // snprintf
 
 #include "mfrc522.h"
 
-#define EEPROM_SIZE 512
-#define LED_PORT PORTC
-#define READ_LED (1<<5)
-#define ACCESS_LED (1<<4)
-#define LED_BITS (READ_LED | ACCESS_LED)
+#define AUX_PORT PORTC
+#define AUX_BITS 0x3F
 
-enum Flags {
-    FLAGS_NONE = 0x00,
-    FLAGS_ADMIN = 0x01,
+class SerialComm {
+public:
+  // 9600 baud, 8 bit, no parity, 1 stop
+  SerialComm() {
+    const unsigned int divider = (F_CPU  / 17 / SERIAL_BAUDRATE) - 1;
+    UBRRH = (unsigned char)(divider >> 8);
+    UBRRL = (unsigned char) divider;
+    UCSRB = (1<<RXEN) | (1<<TXEN);  // read and write
+    UCSRC = (1<<URSEL) /*write-reg*/ | (1<<UCSZ1) | (1<<UCSZ0); /*8bit*/
+  }
+
+  void write(char c) {
+    while ( !( UCSRA & (1<<UDRE)) )  // wait for transmit buffer to be ready.
+      ;
+    UDR = c;
+  }
+
+  // Convenience method.
+  void writeString(const char *str) {
+    while (*str) write(*str++);
+  }
+
+  // Convenience method.
+  void writeHex(char c) {
+    write(to_hex[(unsigned char) c >> 4]);
+    write(to_hex[(unsigned char) c & 0x0f]);
+  }
+
+  inline bool read_ready() {
+    return (UCSRA & (1<<RXC));
+  }
+
+  char read() {
+    while (!read_ready())
+      ;
+    return UDR;
+  }
+private:
+  static const char to_hex[16];
 };
+const char SerialComm::to_hex[] = { '0','1','2','3','4','5','6','7',
+                                    '8','9','a','b','c','d','e','f' };
 
-struct ShortUid {
-    byte uidByte[4];
-    byte flags;   // e.g. 'admin' etc.
-};
+// A line buffer reading from the serial communication line. Provides a
+// nonblocking way to fill a buffer.
+class LineBuffer {
+public:
+  LineBuffer() : pos_(buffer_) { }
 
-#define MAX_UIDS (EEPROM_SIZE - 1) / sizeof(ShortUid)
-
-struct IdStore {
-    byte count;
-    struct ShortUid uid[ MAX_UIDS ];
-};
-
-struct IdStore ee_store EEMEM;
-
-static byte uid_count() {
-    return eeprom_read_byte(&ee_store.count);
-}
-
-
-// Return 'true' if the uid 'to_find' can be found.
-// Obliteterates 'found_buffer'. If the uid has been found, found_buf
-// contains the given uid, otherwise is undefined.
-static bool findUid(const MFRC522::Uid &to_find, ShortUid *found_buf) {
-    if (to_find.size < sizeof(found_buf->uidByte))
-        return false;
-    const byte count = uid_count();
-    for (byte i = 0; i < count; ++i) {
-        eeprom_read_block(found_buf, &ee_store.uid[i], sizeof(*found_buf));
-        if (memcmp(found_buf->uidByte, to_find.uidByte,
-                   sizeof(found_buf->uidByte)) == 0)
-            return true;
+  // Empties serial input buffer and stores in internal buffer.
+  // Returns number of characters if newline reached or buffer full.
+  // Returns '0' while this condition is not yet reached.
+  byte noblockReadline(SerialComm *comm) {
+    const char *end_buf = buffer_ + sizeof(buffer_) - 1;
+    bool newline_seen = false;
+    while (!newline_seen && comm->read_ready() && pos_ < end_buf) {
+      const char c = (*pos_++ = comm->read());
+      newline_seen = (c == '\r' || c == '\n');
     }
-    return false;
+    *pos_ = '\0';  // We always have at least one byte space.
+    if (newline_seen || pos_ >= end_buf) {
+      byte len = pos_ - buffer_;
+      pos_ = buffer_;
+      return len;
+    } else {
+      return 0;
+    }
+  }
+
+  // Returns current line, '\0' terminated.
+  const char *line() const { return buffer_; }
+
+private:
+  char buffer_[32 + 1];
+  char *pos_;
+};
+
+static void printHelp(SerialComm *out) {
+  out->writeString(
+    "? Noisebridge RFID reader protocol v0.1 8/2014\r\n"
+    "? Continuously monitors keypad and RFID. Whenever one of these are read,\r\n"
+    "? sends out a single line, prefixed with 'K' or 'R' followed by the content:\r\n"
+    "? K <digits-typed-in-keypad-ending-with-#>\r\n"
+    "?   or\r\n"
+    "? R <number-of-bytes-hex> <serial-id-from-rfid-as-hex-string>\r\n"
+    "? (Note: keypad not implemented yet)\r\n"
+    "? The prototocol understands a few commands. They are all one-line commands\r\n"
+    "? starting with a single command character. The response can be\r\n"
+    "? one or multiple lines, prefixed with the command character.\r\n"
+    "? Commands:\r\n"
+    "? \t?      This help.\r\n"
+    "? \tP      Ping (to check for aliveness).\r\n"
+    "? \tS<xx>  Set output bits. <xx> is an 8-bit hex number.\r\n");
 }
 
-static bool storeUid(const MFRC522::Uid &to_store, byte flags) {
-    const byte count = uid_count();
-    if (count >= MAX_UIDS || to_store.size < sizeof(ee_store.uid[0].uidByte))
-        return false;
-    eeprom_write_block(to_store.uidByte, &ee_store.uid[count].uidByte,
-                       sizeof(ee_store.uid[count].uidByte));
-    eeprom_write_byte(&ee_store.uid[count].flags, flags);
-    eeprom_write_byte(&ee_store.count, count + 1);
-    return true;
+static void setAuxBits(const char *buffer, SerialComm *out) {
+  int value;
+  if (1 == sscanf(buffer, "S%x", &value)) {
+    value &= AUX_BITS;
+    PORTC = value;
+    char buf[8];
+    snprintf(buf, sizeof(buf), "S%02x\r\n", value);
+    out->writeString(buf);
+  } else {
+    out->writeString("S<invalid>\r\n");
+  }
+}
+
+static void handleCommand(const char *buffer, SerialComm *out) {
+  switch (buffer[0]) {
+  case '?':
+    printHelp(out);
+    break;
+  case 'P':
+    out->writeString("Pong\r\n");
+    break;
+  case 'S':
+    setAuxBits(buffer, out);
+    break;
+  case '\r': case '\n':
+    break;  // ignore spurious newline.
+  default:
+    out->writeString("? Unknown command. Try '?' for help.\r\n");
+  }
+}
+
+static void writeUid(const MFRC522::Uid &uid, SerialComm *out) {
+    out->writeString("R ");
+    out->writeHex((unsigned char) uid.size);
+    out->write(' ');
+    for (int i = 0; i < uid.size; ++i) {
+      out->writeHex(uid.uidByte[i]);
+    }
+    out->writeString("\r\n");
 }
 
 int main() {
-  DDRC = LED_BITS;
+  DDRC = AUX_BITS;
 
   MFRC522 card_reader;
   card_reader.PCD_Init();
 
-  // Init 
-  PORTC |= READ_LED;
-  _delay_ms(100);
-  PORTC &= ~READ_LED;
-
-  PORTC |= ACCESS_LED;
-  _delay_ms(100);
-  PORTC &= ~ACCESS_LED;
-  
   MFRC522::Uid current_uid;
-  ShortUid access_uid;
 
-  if (uid_count() == 0xff) {
-      eeprom_write_byte(&ee_store.count, 0);
-  }
-
-  bool first_time = (uid_count() == 0);
+  SerialComm comm;
+  LineBuffer lineBuffer;
+  comm.writeString("Noisebridge access control outpost. '?' for help.\r\n");
+  int rate_limit = 0;
 
   for (;;) {
+    if (lineBuffer.noblockReadline(&comm) != 0)
+      handleCommand(lineBuffer.line(), &comm);
     if (!card_reader.PICC_IsNewCardPresent())
       continue;
-    if (!card_reader.PICC_ReadCardSerial())
+    if (!card_reader.PICC_ReadCardSerial()) {
+      current_uid.size = 0;
       continue;
+    }
+    if (--rate_limit > 0
+        && current_uid.size == card_reader.uid.size
+        && memcmp(current_uid.uidByte, card_reader.uid.uidByte,
+                  current_uid.size) == 0)
+      continue;
+    rate_limit = 10;
     current_uid = card_reader.uid;
-
-    PORTC |= READ_LED;
-
-    if (first_time) {
-        // The first time, blank memory, the first key we see is the master key
-        storeUid(current_uid, FLAGS_ADMIN);
-        first_time = false;
-    }
-
-    if (findUid(current_uid, &access_uid)) {
-        PORTC |= ACCESS_LED;
-        _delay_ms(100);
-        PORTC &= ~ACCESS_LED;
-    }
-
-    while (card_reader.PICC_ReadCardSerial()
-           && current_uid.size == card_reader.uid.size
-           && memcmp(current_uid.uidByte,
-                     card_reader.uid.uidByte, card_reader.uid.size) == 0) {
-    }
-    PORTC &= ~READ_LED;
+    writeUid(current_uid, &comm);
   }
 }
