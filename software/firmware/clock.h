0d1829723f52badcb4088bb19fa21debde63d868
Henner Zeller <h.zeller@acm.org>
2014-08-28 09:24:50 -0700

    o Make Clock a namespace (if the static methods are inline,   the compiler does not warn about them not being used)

--- software/firmware/clock.h
+++ software/firmware/clock.h
@@ -6,31 +6,32 @@
 #define AVR_CLOCK_H_
 
 #include <avr/io.h>
+#include <stdint.h>
 
 /* A 'clock' interface. To do comparisons, it is cheapest to compare with
- * a constant evaluated number of cycles instead of converting cycles to seconds
- * every time. So do idioms like:
- *  if (Clock::now() - last_cyles < Clock::ms_to_cycles(20)) {}
- * This should be simply a namespace instad of a class, but then the compiler
- * warns about static functions being defined and not used.
+ * a compile-time constant evaluated number of cycles instead of converting
+ * cycles to seconds every time.
+ * So do idioms like:
+ *    if (Clock::now() - last_cyles < Clock::ms_to_cycles(20)) {}
+ * The counter rolls over every 8388ms so only time comparisons up to that
+ * value make sense.
  */
-class Clock {
-public:
-  typedef unsigned short cycle_t;
+namespace Clock {
+  typedef uint16_t cycle_t;
 
-  static void init() {
+  static inline void init() {
     TCCR1B = (1<<CS12) | (1<<CS10);  // clk/1024
   }
 
-  // The timer with aroud 7.8 kHz rolls over the 64k every 8.3 seconds: it
+  // The timer with aroud 7.8 kHz rolls over the 64k every 8.3 seconds: so it
   // makes only sense to do unsigned time comparisons <= 8.3 seconds.
   // Returns clock ticks.
-  static cycle_t now() { return TCNT1; }
+  static inline cycle_t now() { return TCNT1; }
 
   // Converts milliseconds into clock cycles. If you provide a constant
-  // expression, the compiler will be able to replace this with a constant,
-  // otherwise it'll get expensive (division and such).
-  static cycle_t ms_to_cycles(unsigned short ms) {
+  // expression at compile-time, the compiler will be able to replace this
+  // with a constant, otherwise it'll get expensive (division and such).
+  static inline cycle_t ms_to_cycles(uint16_t ms) {
     return ms * (F_CPU / 1024/*prescaler*/) / 1000/*ms*/;
   }
 };
