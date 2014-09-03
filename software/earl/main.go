640a1540f15b89a76967f937f7df22bd99b3fcf0
Henner Zeller <h.zeller@acm.org>
2014-09-03 13:14:41 -0700

    o Update documentation.

--- software/earl/main.go
+++ software/earl/main.go
@@ -9,10 +9,26 @@ import (
 	"os"
 )
 
+// Callback interface to be implemented to receive events generated
+// by terminals.
+// Each method call should return quickly; if you need to do something
+// dependent on time, implement HandleTick()
 type Handler interface {
+	// Initialize. This is called once in the beginning and gets the
+	// TerminalStub connected to the terminal. This allows to trigger
+	// actions, such as writing to the LCD display.
 	Init(t *TerminalStub)
+
+	// HandleKeypress receives each character typed on the keypad.
+	// These are ASCII encoded bytes in the range '0'..'9' and '*' and '#'.
 	HandleKeypress(byte)
+
+	// HandleRFID receives the ID of an RFID card presented to the
+	// terminal. While the card is held in front of the terminal, this
+	// repeats every couple of 100ms.
 	HandleRFID(string)
+
+	// HandleTick is called roughly every 500ms when idle.
 	HandleTick()
 }
 
