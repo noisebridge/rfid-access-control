fae5c700191bd0601a0a2ab39d40a6fd26e0a9a6
Henner Zeller <h.zeller@acm.org>
2014-10-27 20:57:29 -0700

    o Refine test a bit

--- software/earl/authenticator_test.go
+++ software/earl/authenticator_test.go
@@ -8,13 +8,13 @@ import (
 
 func ExpectTrue(t *testing.T, condition bool, message string) {
 	if !condition {
-		t.Errorf(message)
+		t.Errorf("Expected to fail, but didn't: %s", message)
 	}
 }
 
 func ExpectFalse(t *testing.T, condition bool, message string) {
 	if condition {
-		t.Errorf(message)
+		t.Errorf("Expected to succeed, but didn't: %s", message)
 	}
 }
 
@@ -36,31 +36,31 @@ func TestAddUser(t *testing.T) {
 	u := User{Name: "Jon Doe", UserLevel: LevelUser, Codes: []string{"doe123"}}
 	// Can't add with bogus member
 	ExpectFalse(t, auth.AddNewUser("non-existent", u),
-		"Adding new user with non-existent code succeded")
+		"Adding new user with non-existent code.")
 
 	// Proper member adding user.
 	ExpectTrue(t, auth.AddNewUser("root123", u),
-		"Failed to add user with valid member account")
+		"Add user with valid member account")
 
 	// Now, freshly added, we should be able to find the user.
 	found = auth.FindUser("doe123")
 	if found == nil || found.Name != "Jon Doe" {
-		t.Errorf("Didn't find user for pin")
+		t.Errorf("Didn't find user for code")
 	}
 
 	// Let's attempt to set a user with the same code
-	u.Name = "AnotherUser"
 	ExpectFalse(t, auth.AddNewUser("root123", u),
-		"Adding user with code already in use succeeded")
+		"Adding user with code already in use.")
 
+	u.Name = "Another,user;[]funny\"characters '" // Stress-test CSV :)
 	u.Codes[0] = "other123"
 	ExpectTrue(t, auth.AddNewUser("root123", u),
-		"Adding user with disjoint code failed")
+		"Adding another user with unique code.")
 
 	// Ok, now let's see if an new authenticator can make sense of the
-	// file.
+	// file we appended to.
 	auth = NewFileBasedAuthenticator(authFile.Name(), "")
-	ExpectTrue(t, auth.FindUser("root123") != nil, "Missing user root")
-	ExpectTrue(t, auth.FindUser("doe123") != nil, "Missing doe")
-	ExpectTrue(t, auth.FindUser("other123") != nil, "Missing Other")
+	ExpectTrue(t, auth.FindUser("root123") != nil, "Finding root123")
+	ExpectTrue(t, auth.FindUser("doe123") != nil, "Finding doe123")
+	ExpectTrue(t, auth.FindUser("other123") != nil, "Finding other123")
 }
