de89936502cdbabc44325ad55a92b1f5b3f2c357
Olivier Laleu <olivier.laleu@gmail.com>
2015-05-02 18:24:55 -0700

    Update user.go
    Added the package astrotime + geographical coordinates of Noisebridge
    Opening at sunrise for fullTimeUser
    Opening at sunrise + 3 for user

--- software/earl/user.go
+++ software/earl/user.go
@@ -9,6 +9,7 @@ package main
 
 import (
 	"encoding/csv"
+	"github.com/cpucycle/astrotime"
 	"log"
 	"strings"
 	"time"
@@ -35,6 +36,12 @@ const (
 	// absence, or blocked otherwise) - any code should be inactive.
 	// This allows absent users to be kept in the file.
 	LevelHiatus = Level("hiatus")
+)
+
+const (
+//Geographical coordinates of Noisebridge
+LATITUDE = float64(37.762384)
+LONGITUDE = float64(122.419188)
 )
 
 const (
@@ -162,19 +169,29 @@ func (user *User) ExpiryDate(now time.Time) time.Time {
 
 // Returns the interval in hours this user may open doors. Includes from,
 // excludes to [from...to). So (7, 22) means >= 7:00 && < 22
-func (user *User) AccessHours() (from int, to int) {
+func (user *User) AccessHours() (from, to time.Time) {
+	sunrise := astrotime.CalcSunrise(time.Now(), LATITUDE, LONGITUDE)
+	loc, err := time.LoadLocation("Local")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	now := time.Now()
+	zeroHour := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, loc)
+	midnight := time.Date(now.Year(), now.Month(), now.Day(), 23, 59, 59, 0, loc)
+	twentyTwoHour := time.Date(now.Year(), now.Month(), now.Day(), 22, 0, 0, 0, loc)
+	
 	switch user.UserLevel {
 	case LevelMember:
-		return 0, 24 // all access
+		return zeroHour, midnight // all access
 	case LevelFulltimeUser:
-		return 7, 24 // 7:00 .. 23:59
+		return sunrise, midnight // sunrise .. 23:59
 	case LevelUser:
-		return 11, 22 // 11:00 .. 21:59
+		return sunrise.Add(3 * time.Hour()), twentyTwoHour // sunrise+3 .. 21:59
 	}
 	// TODO: for time-restricted users such as users for classes,
 	// we can have custom hours here.
-
-	return 0, 0 // no access.
+return zeroHour, zeroHour // no access.
 }
 
 // Set the auth code to some value (should probably be add-auth-code)
